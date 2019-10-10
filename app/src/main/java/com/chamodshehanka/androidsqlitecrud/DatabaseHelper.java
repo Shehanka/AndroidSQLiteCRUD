0cf5d639e58966f6430468ca6071fc7e8417b77f
Shehanka <hcsperera@outlook.com>
2019-10-10 14:01:05 +0530

    DB Created

--- app/src/main/java/com/chamodshehanka/androidsqlitecrud/DatabaseHelper.java
+++ app/src/main/java/com/chamodshehanka/androidsqlitecrud/DatabaseHelper.java
@@ -8,19 +8,30 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public DatabaseHelper(@Nullable Context context, @Nullable String name,
-                          @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    private static final String DATABASE_NAME = "studentDB";
+    private static final String TABLE_NAME = "student";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "Name";
+    public static final String COL_3 = "Address";
+    public static final String COL_4 = "Marks";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
     }
 
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        sqLiteDatabase.execSQL(
+                "CREATE TABLE " +
+                        TABLE_NAME +
+                        "(ID INTERGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Address TEXT, Marks TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
     }
 }
