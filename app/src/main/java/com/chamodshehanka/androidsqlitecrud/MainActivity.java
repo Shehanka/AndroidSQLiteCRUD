cf288dc4eebede434f0f3a7817629bd2b7314174
Shehanka <hcsperera@outlook.com>
2019-10-10 14:07:20 +0530

    DB created

--- app/src/main/java/com/chamodshehanka/androidsqlitecrud/MainActivity.java
+++ app/src/main/java/com/chamodshehanka/androidsqlitecrud/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseHelper databaseHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        databaseHelper = new DatabaseHelper(this);
     }
 }
