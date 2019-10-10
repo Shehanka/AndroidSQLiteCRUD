0b56579caf086985fdd0bd4b3a7c113de39c3c53
Shehanka <hcsperera@outlook.com>
2019-10-10 14:42:22 +0530

    Insert data method added

--- app/src/main/java/com/chamodshehanka/androidsqlitecrud/DatabaseHelper.java
+++ app/src/main/java/com/chamodshehanka/androidsqlitecrud/DatabaseHelper.java
@@ -1,10 +1,13 @@
 package com.chamodshehanka.androidsqlitecrud;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import com.chamodshehanka.androidsqlitecrud.model.Student;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
@@ -17,7 +20,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
+
     }
 
 
@@ -33,5 +36,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(sqLiteDatabase);
+    }
+
+    public boolean insertData(Student student) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, student.getName());
+        contentValues.put(COL_3, student.getAddress());
+        contentValues.put(COL_4, student.getMarks());
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        return result != -1;
     }
 }
