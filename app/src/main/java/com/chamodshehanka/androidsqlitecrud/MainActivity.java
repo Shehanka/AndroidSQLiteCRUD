27d7c7633ed445207032d977cc4307ca2c3cf542
Shehanka <hcsperera@outlook.com>
2019-10-10 15:18:07 +0530

    Add Student UI done

--- app/src/main/java/com/chamodshehanka/androidsqlitecrud/MainActivity.java
+++ app/src/main/java/com/chamodshehanka/androidsqlitecrud/MainActivity.java
@@ -1,17 +1,21 @@
 package com.chamodshehanka.androidsqlitecrud;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.chamodshehanka.androidsqlitecrud.model.Student;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper databaseHelper;
     EditText txtName;
-    ExtraData txtAddress;
-    ExtraData txtMarks;
+    EditText txtAddress;
+    EditText txtMarks;
     Button btnAddData;
 
 
@@ -20,6 +24,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         databaseHelper = new DatabaseHelper(this);
-        txtName = (EditText)findViewById(R.id.txtName);
+
+        txtName = findViewById(R.id.txtName);
+        txtAddress = findViewById(R.id.txtAddress);
+        txtMarks = findViewById(R.id.txtMarks);
+        btnAddData = findViewById(R.id.btnAddData);
+
+        addStudent();
+    }
+
+    public void addStudent() {
+        btnAddData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isAdded = databaseHelper.insertData(new Student("",
+                        txtName.getText().toString(), txtAddress.getText().toString(),
+                        txtMarks.getText().toString()));
+
+                if (isAdded) {
+                    Toast.makeText(MainActivity.this, "Student Added", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Student Adding failed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
